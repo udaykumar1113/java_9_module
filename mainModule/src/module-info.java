602631b2665d0d05f011fdf83092878b91852ea0
udayk <udaykumar1113@gmail.com>
2019-01-31 17:46:37 -0500

    Created 2 modules where main module accesses other module

---
+++ mainModule/src/module-info.java
@@ -0,0 +1,3 @@
+module mainModule{
+    requires moduleA;
+}
---
+++ moduleA/src/com/uday/pack1/ModuleClass.java
@@ -0,0 +1,7 @@
+package com.uday.pack1;
+
+public class ModuleClass {
+    public void printString(){
+        System.out.println("Printing Module A");
+    }
+}
