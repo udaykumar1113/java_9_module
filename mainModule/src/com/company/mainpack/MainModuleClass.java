7df8b9aeec2d2d8ce5cbdc134b5d5ce012f098dd
udayk <udaykumar1113@gmail.com>
2019-01-31 17:47:15 -0500

    Created 2 modules where main module accesses other module

---
+++ mainModule/src/com/company/mainpack/MainModuleClass.java
@@ -0,0 +1,8 @@
+package com.company.mainpack;
+import com.uday.pack1.ModuleClass;
+class MainModuleClass {
+    public static void main(String args[]){
+        ModuleClass moduleClass=new ModuleClass();
+        moduleClass.printString();
+    }
+}
